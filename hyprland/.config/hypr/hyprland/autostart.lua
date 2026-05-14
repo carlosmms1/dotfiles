e2f45851a1a1aec75505d84837de3592d84a5c2b
Carlos <1carlosmms@gmail.com>
2026-05-14 14:14:03 -0300

    feat: adds waybar configs

--- hyprland/.config/hypr/hyprland/autostart.lua
+++ hyprland/.config/hypr/hyprland/autostart.lua
@@ -1,5 +1,7 @@
 hl.on("hyprland.start", function () 
   -- hl.exec_cmd(terminal)
   -- hl.exec_cmd("nm-applet")
+  hl.exec_cmd("brave", { workspace = "1 silent" })
+  hl.exec_cmd(terminal, { workspace = "2 silent" })
   hl.exec_cmd("waybar & hyprpaper")
 end)
