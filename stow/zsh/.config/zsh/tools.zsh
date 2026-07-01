c9ac96501f509cedf955a726b552b5ee4082a113
Carlos Silva <1carlosmms@gmail.com>
2026-07-01 14:49:19 -0300

    chore: loads zsh tools conditionally

--- stow/zsh/.config/zsh/tools.zsh
+++ stow/zsh/.config/zsh/tools.zsh
@@ -7,6 +7,14 @@ if [ -d "/home/linuxbrew/.linuxbrew" ]; then
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv zsh)"
 fi
 
+if [ -d "/home/.local/bin/uv" ]; then
+  . "$HOME/.local/bin/env"
+fi
+
+if [ -d "/home/.cargo/bin" ]; then
+  . "$HOME/.cargo/env" 
+fi
+
 # Zoxide
 eval "$(zoxide init zsh)"
 alias cd="z"
